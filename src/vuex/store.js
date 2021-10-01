8d9f1d86ab04baded17e547a28ab0e89b8123bf8
Dennis Katilov <katilov.denis@gmail.com>
2021-10-01 17:33:09 +0100

    vuex store

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,9 +1,25 @@
 //import { createApp } from 'vue'
 import { createStore } from 'vuex'
+import axios from "axios";
 
 const store = createStore({
     state: {
         products:[]
+    },
+    mutations: {
+        SET_PRODUCTS_TO_STATE: (state, products) => {
+            state.products = products
+        }
+    },
+    actions: {
+      GET_PRODUCTS_FROM_API({commit}){
+          return axios('http://localhost:3000/products', {
+            method: "GET"
+          })
+              .then((AsyncResponse<any> ) => {
+              commit('SET_PRODUCTS_TO_STATE', products);
+          })
+      }
     },
     getters: {
         PRODUCTS(state){
