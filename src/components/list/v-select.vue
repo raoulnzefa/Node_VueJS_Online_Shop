76b443bc53eb610faa8bdecea239f47222f5476b
Dennis Katilov <katilov.denis@gmail.com>
2021-10-14 23:10:48 +0100

    select categories

---
+++ src/components/list/v-select.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="v-select">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-select",
+  props:{},
+  dat() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
