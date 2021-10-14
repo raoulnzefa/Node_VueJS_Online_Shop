46c5b28d5e4f85738052e461c445c06599ed2596
Dennis Katilov <katilov.denis@gmail.com>
2021-10-14 23:16:33 +0100

    select categories

--- src/components/list/v-select.vue
+++ src/components/list/v-select.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="v-select">
-
+    <div class="options">
+      <span
+        v-for="option in options"
+        :key="option.value"
+      >
+        {{ option.name }}
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "v-select",
-  props:{},
-  dat() {
+  props:{
+    options:{
+      type: Array,
+      default(){
+        return []
+      }
+    }
+  },
+  data() {
     return {}
   }
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
